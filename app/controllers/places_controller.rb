2da809ab2da93949896ca92a3ac1b1328070106f
mikemoriarty213 <mike.moriarty@kellogg.northwestern.edu>
2023-02-18 22:02:02 +0000

    first pass at places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,41 @@
 class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all
+      end
+    
+      def show
+        @place = Place.find_by({ "id" => params["id"] })
+        @posts = Post.where({ "place_id" => @place["id"] })
+      end
+    
+      def new
+        @place = Place.new
+      end
+    
+      def create
+        @place = Place.new
+        @place["name"] = params["company"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
+    
+      def edit
+        @place = Place.find_by({ "id" => params["id"] })
+      end
+    
+      def update
+        @place = Place.find_by({ "id" => params["id"] })
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
+    
+      def destroy
+        @place = Place.find_by({ "id" => params["id"] })
+        @place.destroy
+        redirect_to "/places"
+      end
+
+
 end
